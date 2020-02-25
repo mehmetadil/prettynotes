5af7b3e74cf859e91673b71c70cf1fbcdd94292a
mehmetadil <m.adil.istikbal@gmail.com>
2020-02-25 21:56:17 +0200

    note modeli oluşturuldu

---
+++ app/models/note.rb
@@ -0,0 +1,6 @@
+class Note < ApplicationRecord
+  validates_presence_of :title, :content
+
+  belongs_to :user
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_193927) do
+ActiveRecord::Schema.define(version: 2020_02_25_195423) do
+
+  create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
